c4f16d765db4cb38cb19b133b48a8a952b0c2ed2
ahmedghribi <ahmedghribi51@gmail.com>
2022-05-05 16:55:17 +0100

    first commit

---
+++ src/app/shared/routes/common-layout.routes.ts
@@ -0,0 +1,10 @@
+import { AuthGuard } from './../guards/auth.guard';
+import { Routes } from '@angular/router';
+
+export const CommonLayout_ROUTES: Routes = [
+    {
+        path: 'admin',
+        loadChildren: () => import('../../dashboard/dashboard.module').then(m => m.DashboardModule),
+
+    }
+];
